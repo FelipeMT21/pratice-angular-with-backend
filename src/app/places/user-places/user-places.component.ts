5cf5778b2790ba2b26996201778a7bf4774e85c6
Felipe Silva <joaofelipems21@gmail.com>
2024-09-18 10:01:32 -0300

    Adicionado shared e places.services

--- src/app/places/user-places/user-places.component.ts
+++ src/app/places/user-places/user-places.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { PlacesComponent } from '../places.component';
+import { Place } from '../place.model';
+import { PlacesService } from '../places.service';
 
 @Component({
   selector: 'app-user-places',
@@ -10,5 +12,32 @@ import { PlacesComponent } from '../places.component';
   styleUrl: './user-places.component.css',
   imports: [PlacesContainerComponent, PlacesComponent],
 })
-export class UserPlacesComponent {
+export class UserPlacesComponent implements OnInit {
+  isFetching = signal(false);
+  error = signal('');
+  private placeService = inject(PlacesService);
+  private destroyRef = inject(DestroyRef);
+  places = this.placeService.loadedUserPlaces;
+  
+  ngOnInit(): void {
+    console.log(this.places());
+    this.isFetching.set(true)
+    const subscribe = this.placeService.loadUserPlaces().subscribe({
+      error: (error: Error) => {
+        this.error.set(error.message);
+      },
+      complete: () => this.isFetching.set(false)
+    })
+
+    this.destroyRef.onDestroy(() => {
+      subscribe.unsubscribe();
+    })
+  }
+
+  onDeletedPlace(selectedPlace: Place) {
+    const subscribe = this.placeService.removeUserPlace(selectedPlace).subscribe()
+    this.destroyRef.onDestroy(() => {
+      subscribe.unsubscribe();
+    })
+  }
 }
